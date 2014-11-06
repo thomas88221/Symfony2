e6587e40599dc9f4c0679bba351d168c964bbfeb
thomas <thomas@Koenig-VM.thomas>
2014-11-06 13:27:56 +0100

    Formulaires inscription / connexion / mot de passe oublié

--- src/PDS/LoginBundle/Controller/DefaultController.php
+++ src/PDS/LoginBundle/Controller/DefaultController.php
@@ -13,18 +13,6 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         $this->initService(1, $request);
-        $form = $this->createFormBuilder($this->loginService)
-                     ->add('mail',   'text')
-                     ->add('login',  'text')
-                     ->add('pwd',    'password')
-                     ->add('pwd2',   'password')
-                     ->add('agree',  'checkbox')
-                     ->add('submit', 'submit')
-                     ->getForm();
-        
-        $this->loginService->setForm($form);
-        $this->loginService->validate();
-      
         return $this->render(
           'PDSLoginBundle:Default:index.html.twig',
           $this->loginService->getParams()
@@ -36,5 +24,33 @@ class DefaultController extends Controller
         $this->loginService = $this->container->get('pds_login.loginService');
         $this->loginService->setRequest($request);
         $this->loginService->setType($type);
+        $form = $this->createFormBuilder($this->loginService)
+                     ->add('mail',           'text')
+                     ->add('login',          'text')
+                     ->add('pwd',            'password')
+                     ->add('pwd2',           'password')
+                     ->add('mail_register',  'text')
+                     ->add('login_register', 'text')
+                     ->add('pwd_register',   'password')
+                     ->add('pwd2_register',  'password')
+                     ->add('agree',          'checkbox')
+                     ->add('rememberMe',     'checkbox')
+                     ->add('submit',         'submit')
+                     ->getForm();
+        
+        $this->loginService->setForm($form);
+        $this->loginService->validate();
+    }
+    
+    public function registerAction(Request $request)
+    {
+        $this->initService(2, $request);
+        return $this->redirect($this->generateUrl('pds_login_homepage').'#signup-box');
+    }
+    
+    public function forgetAction(Request $request)
+    {
+        $this->initService(3, $request);
+        return $this->redirect($this->generateUrl('pds_login_homepage').'#forgot-box');
     }
 }
